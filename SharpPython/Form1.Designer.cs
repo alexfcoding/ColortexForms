c28c847cc3be2cefd0d87dbddc5564d86b0526c6
alexfcoding <fedotov1609@gmail.com>
2019-06-07 04:47:57 +0600

    Resize, convert, preview images

--- SharpPython/Form1.Designer.cs
+++ SharpPython/Form1.Designer.cs
@@ -32,32 +32,37 @@
             this.BtnDrawSVG = new System.Windows.Forms.Button();
             this.PictureSVGRender = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ListSourceImg = new System.Windows.Forms.CheckedListBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ListChecked = new System.Windows.Forms.CheckedListBox();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.BtnPreview = new System.Windows.Forms.Button();
+            this.BtnResize = new System.Windows.Forms.Button();
+            this.ListProcessImg = new System.Windows.Forms.CheckedListBox();
             ((System.ComponentModel.ISupportInitialize)(this.PictureSVGRender)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BtnRunPython
             // 
-            this.BtnRunPython.Location = new System.Drawing.Point(12, 783);
+            this.BtnRunPython.Location = new System.Drawing.Point(6, 699);
             this.BtnRunPython.Name = "BtnRunPython";
-            this.BtnRunPython.Size = new System.Drawing.Size(108, 41);
+            this.BtnRunPython.Size = new System.Drawing.Size(101, 41);
             this.BtnRunPython.TabIndex = 0;
-            this.BtnRunPython.Text = "RunPython";
+            this.BtnRunPython.Text = "Convert selected";
             this.BtnRunPython.UseVisualStyleBackColor = true;
             this.BtnRunPython.Click += new System.EventHandler(this.BtnRunPython_Click);
             // 
             // BtnDrawSVG
             // 
-            this.BtnDrawSVG.Location = new System.Drawing.Point(131, 783);
+            this.BtnDrawSVG.Location = new System.Drawing.Point(6, 750);
             this.BtnDrawSVG.Name = "BtnDrawSVG";
-            this.BtnDrawSVG.Size = new System.Drawing.Size(108, 41);
+            this.BtnDrawSVG.Size = new System.Drawing.Size(101, 41);
             this.BtnDrawSVG.TabIndex = 1;
             this.BtnDrawSVG.Text = "DrawSVG";
             this.BtnDrawSVG.UseVisualStyleBackColor = true;
@@ -65,21 +70,43 @@
             // 
             // PictureSVGRender
             // 
-            this.PictureSVGRender.Location = new System.Drawing.Point(259, 31);
+            this.PictureSVGRender.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.PictureSVGRender.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PictureSVGRender.Location = new System.Drawing.Point(245, 33);
             this.PictureSVGRender.Name = "PictureSVGRender";
-            this.PictureSVGRender.Size = new System.Drawing.Size(1047, 793);
+            this.PictureSVGRender.Size = new System.Drawing.Size(1076, 790);
             this.PictureSVGRender.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PictureSVGRender.TabIndex = 2;
             this.PictureSVGRender.TabStop = false;
+            this.PictureSVGRender.Click += new System.EventHandler(this.PictureSVGRender_Click);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.ListProcessImg);
+            this.groupBox1.Controls.Add(this.BtnResize);
+            this.groupBox1.Controls.Add(this.BtnPreview);
+            this.groupBox1.Controls.Add(this.ListSourceImg);
+            this.groupBox1.Controls.Add(this.BtnDrawSVG);
+            this.groupBox1.Controls.Add(this.BtnRefresh);
+            this.groupBox1.Controls.Add(this.BtnRunPython);
             this.groupBox1.Location = new System.Drawing.Point(12, 27);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(227, 85);
+            this.groupBox1.Size = new System.Drawing.Size(227, 797);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Settings";
+            // 
+            // ListSourceImg
+            // 
+            this.ListSourceImg.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ListSourceImg.Location = new System.Drawing.Point(6, 33);
+            this.ListSourceImg.MultiColumn = true;
+            this.ListSourceImg.Name = "ListSourceImg";
+            this.ListSourceImg.ScrollAlwaysVisible = true;
+            this.ListSourceImg.Size = new System.Drawing.Size(215, 304);
+            this.ListSourceImg.TabIndex = 10;
+            this.ListSourceImg.UseCompatibleTextRendering = true;
+            this.ListSourceImg.SelectedIndexChanged += new System.EventHandler(this.ListChecked_SelectedIndexChanged);
             // 
             // menuStrip1
             // 
@@ -126,30 +153,62 @@
             this.aboutToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.aboutToolStripMenuItem.Text = "Help";
             // 
-            // ListChecked
+            // BtnRefresh
             // 
-            this.ListChecked.FormattingEnabled = true;
-            this.ListChecked.Location = new System.Drawing.Point(12, 118);
-            this.ListChecked.Name = "ListChecked";
-            this.ListChecked.Size = new System.Drawing.Size(227, 649);
-            this.ListChecked.TabIndex = 9;
-            this.ListChecked.SelectedIndexChanged += new System.EventHandler(this.ListChecked_SelectedIndexChanged);
+            this.BtnRefresh.Location = new System.Drawing.Point(120, 699);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(101, 41);
+            this.BtnRefresh.TabIndex = 10;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // BtnPreview
+            // 
+            this.BtnPreview.Location = new System.Drawing.Point(6, 652);
+            this.BtnPreview.Name = "BtnPreview";
+            this.BtnPreview.Size = new System.Drawing.Size(215, 41);
+            this.BtnPreview.TabIndex = 11;
+            this.BtnPreview.Text = "Preview";
+            this.BtnPreview.UseVisualStyleBackColor = true;
+            this.BtnPreview.Click += new System.EventHandler(this.BtnPreview_Click);
+            // 
+            // BtnResize
+            // 
+            this.BtnResize.Location = new System.Drawing.Point(120, 750);
+            this.BtnResize.Name = "BtnResize";
+            this.BtnResize.Size = new System.Drawing.Size(101, 41);
+            this.BtnResize.TabIndex = 12;
+            this.BtnResize.Text = "Resize";
+            this.BtnResize.UseVisualStyleBackColor = true;
+            this.BtnResize.Click += new System.EventHandler(this.BtnResize_Click);
+            // 
+            // ListProcessImg
+            // 
+            this.ListProcessImg.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ListProcessImg.FormattingEnabled = true;
+            this.ListProcessImg.Location = new System.Drawing.Point(6, 343);
+            this.ListProcessImg.MultiColumn = true;
+            this.ListProcessImg.Name = "ListProcessImg";
+            this.ListProcessImg.ScrollAlwaysVisible = true;
+            this.ListProcessImg.Size = new System.Drawing.Size(215, 304);
+            this.ListProcessImg.TabIndex = 13;
+            this.ListProcessImg.SelectedIndexChanged += new System.EventHandler(this.ListProcessImg_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1333, 836);
-            this.Controls.Add(this.ListChecked);
             this.Controls.Add(this.PictureSVGRender);
-            this.Controls.Add(this.BtnRunPython);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.BtnDrawSVG);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Pic2Text";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.PictureSVGRender)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -169,7 +228,11 @@
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
-        public System.Windows.Forms.CheckedListBox ListChecked;
+        private System.Windows.Forms.Button BtnRefresh;
+        public System.Windows.Forms.CheckedListBox ListSourceImg;
+        private System.Windows.Forms.Button BtnPreview;
+        private System.Windows.Forms.Button BtnResize;
+        public System.Windows.Forms.CheckedListBox ListProcessImg;
     }
 }
 
--- SharpPython/Form1.cs
+++ SharpPython/Form1.cs
@@ -10,21 +10,25 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Svg;
 using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Threading;
 
 namespace SharpPython
 {
     public partial class Form1 : Form
     {
+        List<String> FileExtensions = new List<string>();
+
         public Form1()
         {
-            InitializeComponent();
-
-            List<String> FileExtensions = new List<string>();
+            InitializeComponent();                        
 
             FileExtensions.Add("*.png");
             FileExtensions.Add("*.jpg");
 
-            FillListBox(ListChecked, @"input", FileExtensions);
+            FillListBox(ListSourceImg, @"prepare", FileExtensions);
         }
 
         private void BtnRunPython_Click(object sender, EventArgs e)
@@ -32,7 +36,16 @@ namespace SharpPython
             string pyPath = @"C:\Users\Александр\AppData\Local\Programs\Python\Python37-32\python.exe";
             string pyScript = @"C:\Users\Александр\source\SharpPython\SharpPython\bin\Debug\main.py";
 
-            
+            System.IO.DirectoryInfo di = new DirectoryInfo("input");
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+
+            PictureSVGRender.Image.Save("input/" + ListSourceImg.GetItemText(ListSourceImg.SelectedItem));
+            Thread.Sleep(200);
+
             ExecutePython(pyPath, pyScript);
         }
         
@@ -75,7 +88,31 @@ namespace SharpPython
 
             RenderSVG(svgPath);
         }
+        
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
 
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
         private void BtnConvert_Click(object sender, EventArgs e)
         {
            
@@ -83,7 +120,43 @@ namespace SharpPython
 
         private void ListChecked_SelectedIndexChanged(object sender, EventArgs e)
         {
-            using (var fromFile = Image.FromFile(@"input/" + ListChecked.GetItemText(ListChecked.SelectedItem)))
+            using (var fromFile = Image.FromFile(@"prepare/" + ListSourceImg.GetItemText(ListSourceImg.SelectedItem)))
+            {
+                PictureSVGRender.Image = new Bitmap(fromFile);
+            }
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            ListSourceImg.Items.Clear();
+            ListProcessImg.Items.Clear();
+            FillListBox(ListSourceImg, @"prepare", FileExtensions);
+            FillListBox(ListProcessImg, @"output", FileExtensions);
+        }
+
+        private void BtnResize_Click(object sender, EventArgs e)
+        {
+            PictureSVGRender.Image = ResizeImage(PictureSVGRender.Image, PictureSVGRender.Image.Width / 2, PictureSVGRender.Image.Height / 2);
+        }
+
+        private void BtnPreview_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureSVGRender_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListProcessImg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (var fromFile = Image.FromFile(@"output/" + ListSourceImg.GetItemText(ListProcessImg.SelectedItem)))
             {
                 PictureSVGRender.Image = new Bitmap(fromFile);
             }
